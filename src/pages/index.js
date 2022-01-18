0c905b19c12327dc9ce7be7a5e2dcc64a55bb5c4
AlexandroMtzG <alex.martinez@absys.com.mx>
2022-01-17 21:20:03 -0600

    saasfrontends-v1

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,29 +16,120 @@ export default function Home() {
         <meta name="twitter:site" content="@alexandromtzg" />
         <meta name="twitter:creator" content="@alexandromtzg" />
         <meta name="twitter:title" content="Blog – alexandro.dev" />
-        <meta name="twitter:description" content="Dev tips, advice and stories from @alexandromtzg." />
+        <meta
+          name="twitter:description"
+          content="Dev tips, advice and stories from @alexandromtzg."
+        />
         <meta name="twitter:image" content={`https://alexandro.dev${twitterCard}`} />
         <meta property="og:url" content="https://alexandro.dev" />
         <meta property="og:type" content="article" />
         <meta property="og:title" content="Blog – Tailwind CSS" />
-        <meta property="og:description" content="Dev tips, advice and stories from @alexandromtzg." />
+        <meta
+          property="og:description"
+          content="Dev tips, advice and stories from @alexandromtzg."
+        />
         <meta property="og:image" content={`https://alexandro.dev${twitterCard}`} />
         <title>Blog - alexandro.dev</title>
         <meta name="description" content="Dev tips, advice and stories from @alexandromtzg." />
       </Head>
-     
+
       <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-        <h1 className="text-3xl leading-9 font-extrabold text-gray-900 tracking-tight sm:text-4xl sm:leading-10 md:text-3xl md:leading-14">
-          A passionate developer blog
-        </h1>
+        {/* <h1 className="text-3xl leading-9 font-extrabold text-gray-900 tracking-tight sm:text-4xl sm:leading-10 md:text-3xl md:leading-14">
+          Blog
+        </h1> */}
         <p className="text-lg leading-7 text-gray-500">
-            I'm <a target="_blank" href="https://twitter.com/alexandromtzg" className="underline text-gray-500 hover:text-gray-600">
-            @AlexandroMtzG</a>, full-stack developer, 
-            I have great admiration for the Tailwind CSS team, 
-            so I'm focused on building a better design for my projects (currently <a className="underline text-gray-500 hover:text-gray-600" href="https://netcoresaas.com" target="_blank">netcoresaas.com</a>).
+          I'm{' '}
+          <a
+            target="_blank"
+            href="https://twitter.com/alexandromtzg"
+            className="underline text-gray-500 hover:text-gray-800"
+          >
+            @AlexandroMtzG
+          </a>
+          , full-stack developer.
         </p>
         <p className="text-lg leading-7 text-gray-500">
-          My favorite tools are <a target="_blank" href="http://aka.ms/net" className="text-purple-600 font-normal hover:text-purple-700 hover:underline">.NET</a>, <a target="_blank" href="https://vuejs.org" className="text-green-500 font-normal hover:text-green-600 hover:underline"> Vue</a>, <a target="_blank" href="https://tailwindcss.com" className="text-teal-500 font-normal hover:text-teal-600 hover:underline"> Tailwind</a>, <a target="_blank" href="https://www.postgresql.org" className="text-blue-600 font-normal hover:text-blue-700 hover:underline"> Postgres</a>, <a target="_blank" href="https://www.heroku.com" className="text-purple-800 font-normal hover:text-purple-900 hover:underline"> Heroku</a> and <a target="_blank" href="https://aws.amazon.com/lightsail" className="text-yellow-400 font-normal hover:text-yellow-500 hover:underline"> Lightsail</a>.
+          My favorite tools are{' '}
+          <a
+            target="_blank"
+            href="http://aka.ms/net"
+            className="text-purple-800 font-normal hover:text-purple-700 hover:underline"
+          >
+            .NET
+          </a>
+          ,{' '}
+          <a
+            target="_blank"
+            href="https://vuejs.org"
+            className="text-green-800 font-normal hover:text-green-800 hover:underline"
+          >
+            {' '}
+            Vue
+          </a>
+          ,{' '}
+          <a
+            target="_blank"
+            href="https://reactjs.org/"
+            className="text-blue-800 font-normal hover:text-blue-800 hover:underline"
+          >
+            {' '}
+            React
+          </a>
+          ,{' '}
+          <a
+            target="_blank"
+            href="https://svelte.dev/"
+            className="text-orange-800 font-normal hover:text-orange-800 hover:underline"
+          >
+            {' '}
+            Svelte
+          </a>
+          ,{' '}
+          <a
+            target="_blank"
+            href="https://tailwindcss.com"
+            className="text-teal-800 font-normal hover:text-teal-800 hover:underline"
+          >
+            {' '}
+            Tailwind
+          </a>
+          ,{' '}
+          <a
+            target="_blank"
+            href="https://www.postgresql.org"
+            className="text-blue-800 font-normal hover:text-blue-700 hover:underline"
+          >
+            {' '}
+            Postgres
+          </a>
+          ,{' '}
+          <a
+            target="_blank"
+            href="https://vercel.com"
+            className="text-gray-800 font-normal hover:text-gray-700 hover:underline"
+          >
+            {' '}
+            Vercel
+          </a>
+          ,{' '}
+          <a
+            target="_blank"
+            href="https://www.heroku.com"
+            className="text-purple-800 font-normal hover:text-purple-900 hover:underline"
+          >
+            {' '}
+            Heroku
+          </a>{' '}
+          and{' '}
+          <a
+            target="_blank"
+            href="https://aws.amazon.com/lightsail"
+            className="text-yellow-800 font-normal hover:text-yellow-500 hover:underline"
+          >
+            {' '}
+            Lightsail
+          </a>
+          .
         </p>
       </div>
       <ul className="divide-y divide-gray-200">
@@ -66,7 +157,7 @@ export default function Home() {
                   <div className="text-base leading-6 font-normal">
                     <Link href={link}>
                       <a
-                        className="text-gray-500 hover:text-gray-600"
+                        className="text-gray-500 hover:text-gray-800"
                         aria-label={`Read "${meta.title}"`}
                       >
                         Read more &rarr;
