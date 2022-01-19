15e008528cd537c2324fd19aa48fb86ff2822f6b
AlexandroMtzG <alex.martinez@absys.com.mx>
2022-01-19 13:17:37 -0600

    logo and icon alts

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,11 @@ import Link from 'next/link'
 import icon from '../img/icon.png'
 import logo from '../img/logo.png'
 
-
 function TailwindMark({ className }) {
-  return (
-    <img className={className} src={icon} />
-  )
+  return <img alt="Icon" className={className} src={icon} />
 }
 function TailwindLogo({ className }) {
-  return (
-    <img className={className} src={logo} />      
-  )
+  return <img alt="Icon" className={className} src={logo} />
 }
 
 export default function Header() {
@@ -24,9 +19,13 @@ export default function Header() {
             <TailwindLogo className="hidden sm:block h-9" />
           </a>
         </Link>
-      </div> 
-       <div className="text-base leading-5">
-        <a target="_blank" href="https://twitter.com/alexandromtzg" className="font-medium text-gray-500 hover:text-gray-700">
+      </div>
+      <div className="text-base leading-5">
+        <a
+          target="_blank"
+          href="https://twitter.com/alexandromtzg"
+          className="font-medium text-gray-500 hover:text-gray-700"
+        >
           Follow me &rarr;
         </a>
       </div>
